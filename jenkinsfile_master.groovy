20183ecd09a51c57325fa2e62461d0b91bb14489
gongxiaocheng <gxcsgdhr@outlook.com>
2020-04-03 12:39:57 +0800

    构建

--- jenkinsfile_master.groovy
+++ jenkinsfile_master.groovy
@@ -10,7 +10,7 @@ stage('maven compile & package') {
         sh ". ~/.bash_profile"
 
         //定义maven java环境
-//        def mvnHome = tool 'maven-3.6.0_master'
+//        def mvnHome = tool 'maven_master'
 //        def jdkHome = tool 'jdk1.8_master'
 //        env.PATH = "${mvnHome}/bin:${env.PATH}"
 //        env.PATH = "${jdkHome}/bin:${env.PATH}"
